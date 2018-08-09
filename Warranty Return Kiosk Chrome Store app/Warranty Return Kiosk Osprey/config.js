d207fec3fe74d565137cabf4dade7e7fed1d4344
ChrisMagnuson <hourglasssand@gmail.com>
2018-08-09 10:46:05 -0400

    Add warranty returns apps

---
+++ Warranty Return Kiosk Chrome Store app/Warranty Return Kiosk Osprey/config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Warranty Return Kiosk Osprey",
+   "homepage": "https://www.tervis.com/store-return?storeid=1010",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": true
+};
